7742cb6c1abff16370827cf10f1294455d2e3d49
BenjaminMassey <benjamin.w.massey@gmail.com>
2020-07-15 19:29:00 -0700

    Made a script to auto-correct radius so that level creation doesn't involve awkward position movement. Made face camera script so do not need to use generic face script and manually drag camera.

---
+++ Assets/Scripts/CorrectRadius.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectRadius : MonoBehaviour
+{
+    public float offset = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject world = GameObject.Find("World");
+        float world_radius = world.transform.localScale.y / 2.0f;
+
+        Quaternion orig_rot = transform.rotation;
+
+        transform.LookAt(world.transform);
+
+        Vector3 p = transform.position;// + (offset * transform.forward);
+
+        float r = Mathf.Sqrt(Mathf.Pow(p.x, 2.0f) + Mathf.Pow(p.y, 2.0f) + Mathf.Pow(p.z, 2.0f));
+        
+        float dist = (r - world_radius) - offset;
+        transform.Translate(Vector3.forward * dist, Space.Self);
+
+        //Debug.Log("World radius: " + world_radius + " | My radius: " + r + " | Moved amount: " + dist + "(Offset " + offset + ")");
+
+        transform.rotation = orig_rot;
+    }
+}
---
+++ Assets/Scripts/FaceCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+
+    private GameObject cam;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(cam.transform);
+    }
+}
